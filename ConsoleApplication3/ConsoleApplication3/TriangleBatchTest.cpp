5365f23f0839d67b5a3a586b1fcee4d965469a4f
zhangyaning2582 <346445259@qq.com>
2018-12-08 17:28:47 +0800

    圆环旋转

--- ConsoleApplication3/ConsoleApplication3/TriangleBatchTest.cpp
+++ ConsoleApplication3/ConsoleApplication3/TriangleBatchTest.cpp
@@ -1,5 +1,11 @@
 #include <GLTools.h>            // OpenGL toolkit
 #include <GLShaderManager.h>    // Shader Manager Class
+#include <GLMatrixStack.h>
+#include <GLFrame.h>
+#include <GLFrustum.h>
+#include <GLGeometryTransform.h>
+#include <GLBatch.h>
+#include <StopWatch.h>
 
 #ifdef __APPLE__
 #include <glut/glut.h>          // OS X version of GLUT
@@ -7,6 +13,8 @@
 #define FREEGLUT_STATIC
 #include <GL/glut.h>            // Windows FreeGlut equivalent
 #endif
+
+GLFrustum viewFrustum;
 
 GLBatch    triangleBatch;
 GLShaderManager    shaderManager;
@@ -20,6 +28,7 @@ GLfloat vVerts[] = { -0.5f, 0.0f, 0.0f,
 void ChangeSize(int w, int h)
 {
 	glViewport(0, 0, w, h);
+	viewFrustum.SetPerspective(35.0f, float(w) / float(h), 1.0f, 1000.0f);
 }
 
 
@@ -29,15 +38,16 @@ void ChangeSize(int w, int h)
 void SetupRC()
 {
 	// Blue background
-	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
-
+	glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
 	shaderManager.InitializeStockShaders();
 
 	// Load up a triangle
 	/*triangleBatch.Begin(GL_TRIANGLES, 3);
 	triangleBatch.CopyVertexData3f(vVerts);
 	triangleBatch.End();*/
-	myCoolObject.BeginMesh(200);
+	gltMakeTorus(myCoolObject, 0.4f, 0.15f, 30, 30);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 }
 
 
@@ -46,12 +56,31 @@ void SetupRC()
 // Called to draw scene
 void RenderScene(void)
 {
+	static CStopWatch rotTimer;
+	float yRot = rotTimer.GetElapsedSeconds() * 60.0f;
 	// Clear the window with current clearing color
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-	GLfloat vRed[] = { 1.0f, 0.0f, 0.0f, 1.0f };
-	shaderManager.UseStockShader(GLT_SHADER_IDENTITY, vRed);
-	triangleBatch.Draw();
+	// Matrix Variables
+	M3DMatrix44f mTranslate, mRotate, mModelview, mModelViewProjection;
+
+	// Create a translation matrix to move the torus back and into sight
+	m3dTranslationMatrix44(mTranslate, 0.0f, 0.0f, -2.5f);
+
+	// Create a rotation matrix based on the current value of yRot
+	m3dRotationMatrix44(mRotate, m3dDegToRad(yRot), 0.0f, 1.0f, 0.0f);
+
+	// Add the rotation to the translation, store the result in mModelView
+	m3dMatrixMultiply44(mModelview, mTranslate, mRotate);
+
+	// Add the modelview matrix to the projection matrix, 
+	// the final matrix is the ModelViewProjection matrix.
+	m3dMatrixMultiply44(mModelViewProjection, viewFrustum.GetProjectionMatrix(), mModelview);
+
+	GLfloat vRed[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	shaderManager.UseStockShader(GLT_SHADER_FLAT, mModelViewProjection, vRed);
+	myCoolObject.Draw();
+
 
 	// Perform the buffer swap to display back buffe
 	glutSwapBuffers();
